56a09ba6c12eb16e62fb5924e0e9b59b20e274a8
TheWebDeveloper7 <161119194+TheWebDeveloper7@users.noreply.github.com>
2026-04-27 11:08:32 +0530

    Add files via upload

--- generate_token.py
+++ generate_token.py
@@ -2,7 +2,7 @@ from kiteconnect import KiteConnect
 
 api_key = "rynalfq2ew07gxmc"
 api_secret = "0kskgjs8t4d42x1i93engxnkenqu1229"
-request_token = "SWuOaRkcw2tPokfwFjkXa6oEgU94U791"
+request_token = "EQcHI9JUCdnZhFGEpT2lRYH8VcX52fj9"
 
 kite = KiteConnect(api_key=api_key)
 
